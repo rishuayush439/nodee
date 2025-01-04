a01a7d910b591a42f7b76a821b44d3f2bf375a25
Ayush Mishra <rishuayush439@gmail.com>
2025-01-04 10:31:14 +0530

    Learning to create server

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name: 'Ayush Mishra',email:'rishu@gmail.com',contact:'8578487823'},
+    {name: 'Rahul',email:'rahul@gmail.com',contact:'8578487873'},
+    {name: 'Krish',email:'krish@gmail.com',contact:'8578487833'}
+]
+module.exports = data;
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const colors = require('colors');
-console.log("Package.json".green);
+const http = require('http');
+const data = require('./data');
+http.createServer((req,resp)=>{
+resp.writeHead(200,{'Content-Type':'application\json'});
+resp.write(JSON.stringify(data));
+resp.end();
+
+}).listen(5000);
 
 
